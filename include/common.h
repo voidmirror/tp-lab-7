1ab68408435a1c76f52bf2c2df16c5cb4292e7eb
Krylova Maria <krylova.maria.2629@gmail.com>
2020-04-28 19:08:03 +0300

    add last files

---
+++ include/common.h
@@ -0,0 +1,16 @@
+//
+// Created by Mary on 28.04.2020.
+//
+
+#ifndef TASK1_COMMON_H
+#define TASK1_COMMON_H
+
+#include <utility>
+
+const std::pair<int, int> ocean_size  {10, 10};
+const int prey_lifetime = 5;
+const int prey_reproducetime = 3;
+const int predator_lifetime = 7;
+const int predator_reproducetime = 4;
+const int predator_faminetime = 4;
+#endif //TASK1_COMMON_H
---
+++ src/cell.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Mary on 15.04.2020.
+//
+#include "cell.h"
+Cell::Cell()
+{
+    ocean = nullptr;
+    coordinates = std::pair<size_t , size_t >(0, 0);
+    object = nullptr;
+}
+void Cell::init(size_t coord_x, size_t coord_y, Ocean* oc)
+{
+    coordinates.first = coord_x;
+    coordinates.second = coord_y;
+    ocean = oc;
+}
+Object* Cell::getObject()
+{
+    return object;
+}
+void Cell::setObject(Object* newObject)
+{
+    object = newObject;
+
+}
+std::pair<size_t, size_t> Cell::getCoordinates()
+{
+    return coordinates;
+}
+
+Ocean* Cell::getOcean() {
+    return ocean;
+}
+
+Cell* Cell::isPreyAround(std::pair<size_t, size_t>coords)
+{
+    return ocean->isPreyAround(coords);
+}
+
+Cell* Cell::isFreeCellAround(std::pair<size_t, size_t> coords)
+{
+    return ocean->isFreeCellAround(coords);
+}
