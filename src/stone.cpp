c6233170f5f9be0e605c5b38e9c09090dc6ea453
Polyukhov Vladislav <vlad.polyukhov@gmail.com>
2020-04-29 17:25:00 +0300

    stone.cpp change

--- src/stone.cpp
+++ src/stone.cpp
@@ -7,7 +7,7 @@ Stone::Stone(Cell* cell) : Object(cell) {
 	live_time = rand() % 99999999 + 1;
 }
 void Stone::live() {
-	if (--live_time <= 0) {
+	if (live_time <= 0) {
 		count--;
 		cell->clearCell();
 		setCell(nullptr);
