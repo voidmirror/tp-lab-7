e10de11075a222b09d6abd96d65cce5b50f3780a
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2020-05-06 00:48:15 +0300

    making it better

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 #include "Ocean.hpp"
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <term.h>
 
 int main(int argc, const char * argv[]) {
     Ocean ocean = Ocean();
+    
+    int preyValue, predValue, stoneValue;
+    cout << "Enter number of preys >> ";
+    cin >> preyValue;
+    cout << "Enter number of predators >> ";
+    cin >> predValue;
+    cout << "Enter number of stones >> ";
+    cin >> stoneValue;
+    
+    ocean.addObj(ObjType::PREDATOR, preyValue);
+    ocean.addObj(ObjType::PREY, predValue);
+    ocean.addObj(ObjType::STONE, stoneValue);
 
-    ocean.addObj(ObjType::PREDATOR, 30);
-    ocean.addObj(ObjType::PREY, 100);
-    ocean.addObj(ObjType::STONE, 1);
-
-    while (true)
-    {
+    while (true) {
+        #ifdef _WIN32
+               system("cls");
+        #else
+               system("clear");
+        #endif
         ocean.run();
         int *res = ocean.print();
-        if (res[0] == 0 || res[1] == 0)
-        {
-            break;
+        char foo;
+        cout << "Press enter to continue . . .";
+        foo = cin.get();
+        if (foo == 10){
+            if (res[0] == 0 || res[1] == 0)
+            {
+                if (res[0] == 0) {
+                    cout << "All preys died" << endl;
+                }
+                else {
+                    cout << "All predators died" << endl;
+                }
+            }
+            else{
+                #ifdef _WIN32
+                    system("pause");
+                #else
+                    system("read");
+                #endif
+            }
         }
     }
-
 }
