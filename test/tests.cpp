a05ed29b3efdbf1879997365aa870b0b1cecb427
voidmirror <49756045+voidmirror@users.noreply.github.com>
2020-06-25 07:36:56 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,41 @@
 #include "gtest/gtest.h"
+#include "Cell.h"
+#include "Object.h"
+
+TEST(lab7, task1)	//done
+{
+	Cell *cell = new Cell();
+	EXPECT_EQ(0, (*cell).isEmpty());
+}
+
+TEST(lab7, task2)	//done
+{
+	Object *obj = new Object(3);
+	EXPECT_EQ(6, (*obj).get_hp());
+}
+
+TEST(lab7, task3)	//done
+{
+	Object *obj = new Object(1);
+	EXPECT_EQ(1, (*obj).get_hp());
+}
+
+TEST(lab7, task4)	//done
+{
+	Object *obj = new Object(2);
+	EXPECT_EQ(4, (*obj).get_hp());
+}
+
+
+TEST(lab7, task5)	//done
+{
+	Object *obj = new Object(4);
+	EXPECT_EQ(0, (*obj).get_fertility());
+}
+
+TEST(lab7, task6)	//done
+{
+	Object *obj = new Object(4);
+	(*obj).set_fertility(4);
+	EXPECT_EQ(4, (*obj).get_fertility());
+}
